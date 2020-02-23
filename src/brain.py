39833bfed5c75219a6b2aea1f22a2920fdecc634
Kaleb <kaleb.goering@gmail.com>
2020-02-23 15:47:28 -0700

    Added basic helper functions

---
+++ src/brain.py
@@ -0,0 +1,62 @@
+import yaml
+import mysql.connector as mysql
+
+# Globals
+# Apis to load
+api_list = []
+
+
+def load_config(filename):
+    try:
+        with open(filename, 'r') as filestream:
+            yaml_data = yaml.load(filestream, Loader=yaml.FullLoader)
+            return yaml_data
+    except FileNotFoundError:
+        print("\nNo such config file")
+        return {}
+
+
+def load_mysql(db_login):
+    try:
+        db_conn = mysql.connect(
+            host=db_login['dbhost'],
+            user=db_login['user'],
+            passwd=db_login['pass'],
+            database=db_login['dtbs'])
+        return db_conn
+    except:
+        return None
+
+
+def load_user(db_conn, username):
+    cursor = db_conn.cursor()
+    username = username.lower()
+    cursor.execute("SELECT * FROM users where name = '%s'" % username)
+    data = cursor.fetchone()
+
+    if not data:
+        return (None, None)
+    else:
+        user = {'name': data[0].title()}
+        return (user, data[1].title())
+
+
+def load_ai(db_conn, ai_name):
+    cursor = db_conn.cursor()
+    cursor.execute("SELECT * FROM ais where name = '%s'" % ai_name)
+    data = cursor.fetchone()
+
+    if not data:
+        return None
+    else:
+        return {'name': data[0].title(), 'sex': data[1].title()}
+
+
+def load_apis(apis):
+    api_dict = []
+
+    for api in apis:
+        funct, keys = api()
+        api_dict.append((funct, keys))
+
+    return api_dict
