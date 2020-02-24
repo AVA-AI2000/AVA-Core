68ed04489d3b88ef21fe945783afb2cbd031621e
Kaleb <kaleb.goering@gmail.com>
2020-02-23 17:33:56 -0700

    Updated TextIO.write()

--- src/skills/AVAIO.py
+++ src/skills/AVAIO.py
@@ -1,6 +1,8 @@
 class TextIO:
-    def write(self, message):
-        print('AVA> ' + message)
+    def write(self, message, ai={'name': 'AVA'}):
+        messages = message.split('\n')
+        for mes in messages:
+            print(ai['name'] + '> ' + mes)
 
     def read(self):
         message = input('> ').lower()
